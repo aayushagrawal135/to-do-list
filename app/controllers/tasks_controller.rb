b33ace9aa88e9fb730906d95c983b1e1a5083bf5
aayush <aayushagrawal135@gmail.com>
2019-06-16 17:41:34 +0530

    To do list - draft

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,39 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to({ action: "show", id: @task.id })
+  end
+
+  def edit
+    @task  = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes!(task_params)
+    redirect_to({ action: "show", id: @task.id })
+  end
+
+  def destroy
+    Task.find(params[:id]).destroy
+    redirect_to(root_path)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+end
